0c1c332091ea2ca035441740fd32db648c61758e
HCLonely <h1606051253@gmail.com>
2021-07-19 19:06:17 +0800

    update bilibili-medal

--- bilibili_medal.user.js
+++ bilibili_medal.user.js
@@ -1,22 +1,25 @@
 // ==UserScript==
 // @name         bilibili勋章常亮
 // @namespace    bilibili-medal
-// @version      0.1
+// @version      0.2
 // @description  保持bilibili直播粉丝勋章常亮
 // @author       HCLonely
 // @include      https://link.bilibili.com/p/center/index
 // @run-at       document-end
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js
 // @require      https://cdn.jsdelivr.net/npm/js-cookie@2.2.1/src/js.cookie.min.js
+// @require      https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.all.min.js
 // @connect      api.live.bilibili.com
 // @grant        GM_xmlhttpRequest
 // @grant        GM_getValue
 // @grant        GM_setValue
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
 /* global $, Cookies, FormData */
 (function () {
   'use strict'
+  const whiteList = GM_getValue('whiteList') || []
   if (window.location.href === 'https://link.bilibili.com/p/center/index#/user-center/wearing-center/my-medal') {
     const find = setInterval(async () => {
       if ($('.page-title').length > 0) {
@@ -26,12 +29,13 @@
           $('.text-ctnr').remove()
           $('table.center-grid>thead>tr').html('<td data-v-3895bb76="" width="160" style="padding-left: 60px;">主播昵称</td><td data-v-3895bb76="" width="160">直播间</td><td data-v-3895bb76="" width="120">签到结果</td>')
           $('table.center-grid>tbody').html('')
-          const rooms = await getroomsId()
-          for (let i = 0; i < rooms.length; i++) {
-            if (rooms[i][1] < 100000) {
-              rooms[i][1] = await toLongId(rooms[i][1])
+          const allRooms = await getroomsId()
+          for (let i = 0; i < allRooms.length; i++) {
+            if (allRooms[i][1] < 100000) {
+              allRooms[i][1] = await toLongId(allRooms[i][1])
             }
           }
+          const rooms = allRooms.filter(e => !whiteList.includes(e[1]))
           let i = 0
           $('nav.tabnav').append(`<section class="tabnav-item"><div class="tabnav-content">签到进度：${rooms.length} / <span id="sign-progress" class="tabnav-tip plain">${i}</span></div></section>`)
           for (const room of rooms) {
@@ -93,7 +97,7 @@
         responseType: 'json',
         onload: data => {
           if (data.response.code === 0) {
-            resolve(data.response.data.fansMedalList.filter(e => e.roomid && e.today_intimacy < 100).map(e => [e.uname, e.roomid]).filter(e => e[1]))
+            resolve(data.response.data.fansMedalList.filter(e => e.roomid && e.today_intimacy < 100 && !whiteList.includes(e.roomid)).map(e => [e.uname, e.roomid]).filter(e => e[1]))
           } else {
             resolve(false)
           }
@@ -137,4 +141,24 @@
       })
     })
   }
+  GM_registerMenuCommand('设置白名单',()=>{
+    const whiteList = GM_getValue('whiteList') || []
+    Swal.fire({
+      title: '设置白名单',
+      input: 'textarea',
+      inputValue: whiteList.join(','),
+      showCancelButton: true,
+      confirmButtonText: '保存',
+      cancelButtonText: '取消'
+    }).then(({value}) => {
+      if(value){
+        GM_setValue('whiteList', value.split(','))
+        Swal.fire(
+          '保存成功',
+          '',
+          'success'
+        )
+      }
+    })
+  })
 })()
